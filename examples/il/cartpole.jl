23cdfd2431db1c5ad035214c6f8f392ae3425914
Arec <jamgochian95@gmail.com>
2023-03-22 21:05:09 -0700

    committing cartpole before updating softq and iqlearn functions

--- examples/il/cartpole.jl
+++ examples/il/cartpole.jl
@@ -1,6 +1,4 @@
-using Debugger, Revise #remove
 using Crux, Flux, POMDPGym, Random, POMDPs, BSON
-
 
 ## Cartpole
 mdp = GymPOMDP(:CartPole, version = :v0)
@@ -13,29 +11,8 @@ V() = ContinuousNetwork(Chain(Dense(4, 64, relu), Dense(64, 64, relu), Dense(64,
 A() = DiscreteNetwork(Chain(Dense(4, 64, relu), Dense(64, 64, relu), Dense(64, length(as))), as)
 SA() = SoftDiscreteNetwork(Chain(Dense(4, 64, relu), Dense(64, 64, relu), Dense(64, length(as))), as;α=Float32(1.))
 
-
 # Fill a buffer with expert trajectories
 expert_trajectories = BSON.load("examples/il/expert_data/cartpole.bson")[:data]
-
-# IQLearn ΔN, c_opt epochs hyperparams
-mix = [(1,1),(1,5), (4,5), (4,10), (20,20), (20,50)]
-𝒮_iqls = [OnlineIQLearn(π=SA(), 𝒟_demo=expert_trajectories, S=S, γ=γ, N=10000, ΔN=dn, log=(;period=100), c_opt=(;epochs=e)) for (dn,e) in mix]
-[@time solve(i, mdp) for i in 𝒮_iqls]
-p = plot_learning(𝒮_iqls, title = "CartPole-V0 IQL Tradeoff Curves", 
-    labels = ["IQL ΔN=($dn),ep=($e)" for (dn,e) in mix])
-Crux.savefig(p, "examples/il/cartpole_iqlearn_dne_tradeoffs.pdf")
-
-# IQLearn λ_gp, α_reg hyperparams
-λ_gps = Float32[1, 0.1, 0.01, 0.]
-α_regs = Float32[100, 1, 0.1]
-mix = Iterators.product(λ_gps,α_regs)
-𝒮_iqls = [OnlineIQLearn(π=SA(), 𝒟_demo=expert_trajectories, 
-    S=S, γ=γ, λ_gp=i, α_reg=j, N=10000, ΔN=1, 
-    log=(;period=100), c_opt=(;epochs=1)) for (i,j) in mix]
-[@time solve(i, mdp) for i in 𝒮_iqls]
-p = plot_learning(𝒮_iqls, title = "CartPole-V0 IQL Tradeoff Curves", 
-    labels = ["IQL λ_gp=($i),α_reg=($j)" for (i,j) in mix])
-Crux.savefig(p, "examples/il/cartpole_iqlearn_reg_tradeoffs.pdf")
 
 # Solve with PPO-GAIL
 𝒮_gail = OnPolicyGAIL(D=Disc(), γ=γ, gan_loss=GAN_BCELoss(), 𝒟_demo=expert_trajectories, solver=PPO, π=ActorCritic(A(), V()), S=S, 
@@ -47,7 +24,8 @@ solve(𝒮_gail, mdp)
 solve(𝒮_bc, mdp)
 
 # Solve with IQ-Learn
-𝒮_iql = OnlineIQLearn(π=SA(), 𝒟_demo=expert_trajectories, S=S, γ=γ, N=40000, ΔN=200, log=(;period=50))
+𝒮_iql = OnlineIQLearn(π=SA(), 𝒟_demo=expert_trajectories, S=S, γ=γ, N=10000, ΔN=1, 
+    c_opt=(;epochs=1),reg=false,gp=false, log=(;period=50))
 solve(𝒮_iql, mdp)
 
 # Plot true rewards
--- examples/rl/cartpole.jl
+++ examples/rl/cartpole.jl
@@ -9,18 +9,6 @@ A() = DiscreteNetwork(Chain(Dense(Crux.dim(S)..., 64, relu), Dense(64, 64, relu)
 V() = ContinuousNetwork(Chain(Dense(Crux.dim(S)..., 64, relu), Dense(64, 64, relu), Dense(64, 1)))
 SoftA(α::Float32) = SoftDiscreteNetwork(Chain(Dense(Crux.dim(S)..., 64, relu), Dense(64, 64, relu), Dense(64, length(as))), as;α=α)
 
-# collection and c_opt_epoch optimization
-ΔNs=[1,2,4]
-epochs = [1,5,10,50]
-mix = Iterators.product(ΔNs,epochs)  
-𝒮_sqls_2 = [SoftQ(π=SoftA(Float32(0.5)), S=S, N=10000, 
-    ΔN=dn, c_opt=(;epochs=e), interaction_storage=[]) for (dn,e) in mix]
-π_sqls_2 = [@time solve(x, mdp) for x in 𝒮_sqls_2]
-p = plot_learning(𝒮_sqls_2, title = "CartPole-V0 SoftQ Tradeoff Curves", 
-    labels = ["SQL ΔN=($dn),ep=($e)" for (dn,e) in mix])
-Crux.savefig(p, "examples/rl/cartpole_soft_q_tradeoffs.pdf")
-
-
 # Solve with REINFORCE (~2 seconds)
 𝒮_reinforce = REINFORCE(π=A(), S=S, N=10000, ΔN=500, a_opt=(epochs=5,), interaction_storage=[])
 @time π_reinforce = solve(𝒮_reinforce, mdp)
@@ -38,26 +26,22 @@ Crux.savefig(p, "examples/rl/cartpole_soft_q_tradeoffs.pdf")
 @time π_dqn = solve(𝒮_dqn, mdp)
 
 # Solve with SoftQLearning w/ varying α (~12 seconds)
-αs = Vector{Float32}([1,0.5,0.2,0.1])
-𝒮_sqls = [SoftQ(π=SoftA(α), S=S, N=10000, interaction_storage=[]) for α in αs]
-π_sqls = [@time solve(𝒮_sqls[i], mdp) for i=1:length(αs)]
+𝒮_sql = SoftQ(π=SoftA(Float32(0.1)), S=S, N=10000, 
+    ΔN=1, c_opt=(;epochs=5), interaction_storage=[])
+@time π_sql = solve(𝒮_sql, mdp)
 
 # Plot the learning curve
-p = plot_learning([𝒮_reinforce, 𝒮_a2c, 𝒮_ppo, 𝒮_dqn, 𝒮_sqls...], title = "CartPole-V0 Training Curves", 
-    labels = ["REINFORCE", "A2C", "PPO", "DQN", ["SQL ($i)" for i in αs]...])
+p = plot_learning([𝒮_reinforce, 𝒮_a2c, 𝒮_ppo, 𝒮_dqn, 𝒮_sql], title = "CartPole-V0 Training Curves", 
+    labels = ["REINFORCE", "A2C", "PPO", "DQN", "SoftQ" ])
 Crux.savefig(p, "examples/rl/cartpole_training.pdf")
 
 # Produce a gif with the final policy
 gif(mdp, π_ppo, "cartpole_policy.gif", max_steps=100)
 
 
-
-
-
-
 ## Optional - Save data for imitation learning
 # using BSON
-# s = Sampler(mdp, 𝒮_dqn.π, max_steps=100, required_columns=[:t])
+# s = Sampler(mdp, 𝒮_dqn.agent, max_steps=100, required_columns=[:t])
 # 
 # data = steps!(s, Nsteps=10000)
 # sum(data[:r])/100
